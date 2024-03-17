1d4de2da8f81d1a67f7d93be881d4388694fbad4
Bdnature <84246065+Quddos@users.noreply.github.com>
2024-03-17 10:00:47 -0700

    inplementing functionality

---
+++ Day 2/old/script.js
@@ -0,0 +1,48 @@
+// console.log("Hello world");
+
+// let age = 15
+
+// age = 20
+// console.log(age)
+
+
+// const salary = 8000
+
+// // salary = 10000
+
+// console.log(salary)
+
+// let nameOfBootcamp = "webinteger "
+
+// console.log(nameOfBootcamp)
+
+// const total = 30
+// console.log(total)
+
+// const amIaMan = true
+// console.log(amIaMan)
+
+// let age = null
+// console.log(age)
+
+//COMMENT
+//Non - premitive
+// const students = {
+//     age: 20,
+//     fName: "Quddus",
+//     lNmae: "bajaj"
+// }
+
+// console.log(students)
+// console.log(students.lNmae)
+// // challenge: Access and display only the age of the 
+// // students object
+
+const evenNumbers = [2, 4, 6, 8]//0 1 2 3
+console.log(evenNumbers)
+// // challenge: Access and display only the second item of the 
+// // evenNumbers array
+console.log(evenNumbers[3])
+//Opertor
+//type conversion
+//implicit explicit
