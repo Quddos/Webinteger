936dc9afc4ab13aa2dd5a5f3d742414d21e3a259
Bdnature <84246065+Quddos@users.noreply.github.com>
2024-03-18 01:02:16 -0700

    day 3

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+
+import {add} from "./function.js"
+const result = add(2,3)
+console.log(result)
+
+// connecting t
+
+
 const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("")
+const addButtonEl = document.getElementById("add-button")
+
+// when a user aclick the add button we want 
+// something to happen
+
+addButtonEl.addEventListener("click", function(){
+    // console.log("butter")
+    // console.log(inputFieldEl.value)
+    let inputValue = inputFieldEl.value
+    console.log(inputValue)
+})
+
